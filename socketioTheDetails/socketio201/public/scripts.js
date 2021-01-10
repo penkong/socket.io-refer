a99f833814687a8416d81abb90b9cf2fb0a84cfa
mazdak <nazemi.works@gmail.com>
2021-01-10 12:22:34 +0400

    more on

--- socketioTheDetails/socketio201/public/scripts.js
+++ socketioTheDetails/socketio201/public/scripts.js
@@ -1,36 +1,35 @@
-const socket = io('http://localhost:9000'); // the / namespace/endpoint
-const socket2 = io('http://localhost:9000/admin') //the /admin namespace
+const socket = io('http://localhost:9000') // the / namespace/endpoint
+const socketAdmin = io('http://localhost:9000/admin') //the /admin namespace
 console.log(socket.io)
-socket.on('connect',()=>{
-    console.log(socket.id)
+socket.on('connect', () => {
+  console.log(socket.id)
 })
 
-socket2.on('connect',()=>{
-    console.log(socket2.id)
+socketAdmin.on('connect', () => {
+  console.log(socketAdmin.id)
 })
 
-socket.on('welcome',(msg)=>{
-    console.log(msg)
+socket.on('welcome', msg => {
+  console.log(msg)
 })
-socket2.on('welcome',(msg)=>{
-    console.log(msg)
-})
-
-
-socket.on('messageFromServer',(dataFromServer)=>{
-    console.log(dataFromServer);
-    socket.emit('dataToServer',{data: "Data from the Client!"})
+socketAdmin.on('welcome', msg => {
+  console.log(msg)
 })
 
-document.querySelector('#message-form').addEventListener('submit',(event)=>{
-    event.preventDefault();
-    const newMessage = document.querySelector('#user-message').value;
-    socket.emit('newMessageToServer',{text: newMessage})
+socket.on('messageFromServer', dataFromServer => {
+  console.log(dataFromServer)
+  socket.emit('dataToServer', { data: 'Data from the Client!' })
 })
 
-socket.on('messageToClients',(msg)=>{
-    console.log(msg)
-    document.querySelector('#messages').innerHTML += `<li>${msg.text}</li>`
+document.querySelector('#message-form').addEventListener('submit', event => {
+  event.preventDefault()
+  const newMessage = document.querySelector('#user-message').value
+  socket.emit('newMessageToServer', { text: newMessage })
+})
+
+socket.on('messageToClients', msg => {
+  console.log(msg)
+  document.querySelector('#messages').innerHTML += `<li>${msg.text}</li>`
 })
 
 // socket.on('ping',()=>{
